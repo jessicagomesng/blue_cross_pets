52d58669ab2a83e6018db27d91d2b74ab23a4b34
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-04-15 19:11:40 +0000

    added choose_animal

--- lib/blue_cross_pets/cli.rb
+++ lib/blue_cross_pets/cli.rb
@@ -5,6 +5,26 @@ class BlueCrossPets::CLI
   def call
     puts "Welcome to the Blue Cross Pet Shelter!"
     puts "We heard you're interested in adopting a furry friend."
+    choose_animal
+  end 
+  
+  def choose_animal 
+    puts "To learn more about our adoptable dogs, type 'dogs'. To learn more about our adoptable cats, type 'cats'. To exit, type 'exit'." 
+    input = gets.strip
+    if input == "dogs"
+      puts "Woof! Fetching dogs..."
+    elsif input == "cats"
+      puts "Purrfect! Fetching cats..."
+    elsif input == "exit"
+      goodbye
+    else
+      puts "Sorry, we didn't understand that! Please type 'dogs' to learn more about dogs, 'cats' to learn more about cats, or 'exit' to exit."
+      input = gets.strip
+    end 
+  end 
+  
+  def goodbye
+    puts "Thanks for stopping by! Have a great day!"
   end 
   
 end
