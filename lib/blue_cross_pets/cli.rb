daf6ff0b82616141f82d6b9000481a838476ae83
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-04-15 19:16:38 +0000

    continued to add call methods

--- lib/blue_cross_pets/cli.rb
+++ lib/blue_cross_pets/cli.rb
@@ -6,6 +6,7 @@ class BlueCrossPets::CLI
     puts "Welcome to the Blue Cross Pet Shelter!"
     puts "We heard you're interested in adopting a furry friend."
     choose_animal
+    choose_pet
   end 
   
   def choose_animal 
@@ -13,14 +14,22 @@ class BlueCrossPets::CLI
     input = gets.strip
     if input == "dogs"
       puts "Woof! Fetching dogs..."
+      #depicts choose_pet for dog
     elsif input == "cats"
-      puts "Purrfect! Fetching cats..."
+      puts "Meow! Fetching cats..."
+      #depicts choose_pet for cat
     elsif input == "exit"
       goodbye
     else
       puts "Sorry, we didn't understand that! Please type 'dogs' to learn more about dogs, 'cats' to learn more about cats, or 'exit' to exit."
       input = gets.strip
     end 
+  end 
+  
+  def choose_pet 
+    puts "Purrfect choice! Here is a list of our available #{pet_type}s: "
+    #numbered list of all available pet types, based off scraper data 
+    #human inputs which cat they want. Otherwise, they want to go back & recall choose_animal
   end 
   
   def goodbye
