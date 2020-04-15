5f1178427b9797c6c0c1503bf2e342c0b420fc95
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-04-15 14:01:10 +0000

    testing

--- lib/blue_cross_pets.rb
+++ lib/blue_cross_pets.rb
@@ -3,4 +3,10 @@ require "blue_cross_pets/version"
 module BlueCrossPets
   class Error < StandardError; end
   # Your code goes here...
+  class Instruct
+    def initial_instruction
+    puts 'This is a test!'
+  end
+end 
+
 end
