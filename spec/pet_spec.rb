b193674f8de70af99ece9922adeac051035c62a1
Jessica Gomes-Ng <jessicagomesng@gmail.com>
2020-04-18 17:12:11 +0000

    adding test details

---
+++ spec/pet_spec.rb
@@ -0,0 +1,87 @@
+require "spec_helper"
+
+#RSpec.describe BlueCrossPets::Pet do
+#  describe ".create_from_index" do
+#   it "uses the Scraper class to create new dogs with the correct name and location." do
+#     BlueCrossPets::Dog.class_variable_set(:@@all, [])
+#     BlueCrossPets::Dog.create_from_collection(student_index_array)
+#     expect(Student.class_variable_get(:@@all).first.name).to eq("Alex Patriquin")
+#   end
+# end
+#end
+
+RSpec.describe BlueCrossPets::Dog do
+
+  let!(:dog_index_array) {[{:name=>"Pepper", :age=>"3 years"},
+  {:name=>"Fido", :age=>"5 years"},
+  {:name=>"Woofer", :age=>"1 year"},
+  {:name=>"Skittles", :age=>"13 years"},
+  {:name=>"Hershey", :age=>"5 years"},
+  {:name=>"Maggie", :age=>"14 years"}]}
+
+  #let!(:dog_attribute_hash) {{:gender=>"male",
+  #:availability=>"available",
+  #:breed_and_colour=>"Cavalier King Charles Spaniel, tricolour",
+  #:can_live_with=>"Dogs, children",
+  #:reference=>"123456",
+  #:profile_url=>"https://www.bluecross.org.uk"
+  #:bio=>"I'm super cute!"}}
+
+  let!(:dog) {BlueCrossPets::Dog.new({:name => "Peety", :age => "14 years"})}
+  after(:each) do
+    BlueCrossPets::Dog.class_variable_set(:@@all, [])
+  end
+
+  it 'inherits from the Pet class' do
+    expect(BlueCrossPets::Dog.methods).to include(create_from_index)
+  end
+
+  context 'ClassMethods' do
+    describe 'initialize' do
+      it "initializes with a hash and sets the dog's attributes using the hash's key/value pairs" do
+        expect{BlueCrossPets::Dog.new({:name => "Peaches", :age => "4 years", :availability => "reserved"})}.to_not raise_error
+        expect(dog.name).to eq("Peety")
+        expect(dog.age).to eq("14 years")
+      end
+
+      it "adds that dog the Dog class' collection of all existing dogs, stored in the @@all class variable." do
+        expect(BlueCrossPets::Dog.class_variable_get(:@@all).first.name).to eq("Peety")
+      end
+    end
+
+    describe '.all' do
+      it 'returns an array of all listed dogs' do
+      expect(BlueCrossPets::Dog.all).to be_an(Array)
+      expect(BlueCrossPets::Dog.all).first.to be_a(BlueCrossPets::Dog)
+      end
+    end
+
+  end
+
+end
+
+RSpec.describe BlueCrossPets::Cat do
+  context 'ClassMethods' do
+    describe 'initialize' do
+      it "initializes with a hash and sets the cat's attributes using the hash's key/value pairs" do
+        expect{BlueCrossPets::Cat.new({:name => "Maddie", :age => "11 years"})}.to_not raise_error
+      end
+    end
+
+    describe '.all' do
+      it 'returns an array of all listed cats' do
+      expect(BlueCrossPets::Cat.all).to be_an(Array)
+      expect(BlueCrossPets::Cat.all).first.to be_a(BlueCrossPets::Cat)
+      end
+    end
+  end
+
+end
+
+#  Pet expectations:
+#    assign attributes to animals
+
+#  Dog expectations: (currently has the aility to do this but it doesnt get called here, it gets called in cli class)
+#    create dog objects
+#    inherits from Pet class
+#    makes a master list of all dogs
--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -1,30 +1,48 @@
-require "spec-helper"
-
-RSpec.describe BlueCrossPets::Dog do
-
-  describe '.all' do
-    it 'returns an array of all listed dogs' do
-    expect(BlueCrossPets::Dog.all).to be_an(Array)
-    expect(BlueCrossPets::Dog.all).first.to be_a(BlueCrossPets::Dog)
-    end
-  end
-
-end
-
-RSpec.describe BlueCrossPets::Cat do
-
-  describe '.all' do
-    it 'returns an array of all listed cats' do
-    expect(BlueCrossPets::Cat.all).to be_an(Array)
-    expect(BlueCrossPets::Cat.all).first.to be_a(BlueCrossPets::Cat)
-    end
-  end
-
-end
+require "spec_helper"
 
 RSpec.describe BlueCrossPets::Scraper do
-    it 'correctly scrapes names, breed, gender, age, profile URL, and availability from the Blue Cross website' do
-      expect(BlueCrossPets::Dog.)
+  #These tests are based on current data from the Blue Cross website.
+  #Because this data is updated all of the time, these test links will need to be updated based off current data from https://www.bluecross.org.uk/rehome/dog".
+
+  describe "#scrape_index" do
+    it 'is a class method that correctly scrapes names, breed, gender, age, profile URL, and availability from Blue Cross and returns an array of hashes for each animal' do
+      dog_index_url = "https://www.bluecross.org.uk/rehome/dog"
+      scraped_dogs = BlueCrossPets::Scraper.scrape_index(dog_index_url)
+      expect(scraped_dogs).to be_a(Array)
+      expect(scraped_dogs.first).to have_key(:name)
+      expect(scraped_dogs.first).to have_key(:breed)
+      expect(scraped_dogs.first).to have_key(:gender)
+      expect(scraped_dogs.first).to have_key(:age)
+      expect(scraped_dogs.first).to have_key(:profile_url)
+      expect(scraped_dogs.first).to have_key(:availability)
+
+      cat_index_url = "https://www.bluecross.org.uk/rehome/cat"
+      scraped_cats = BlueCrossPets::Scraper.scrape_index(cat_index_url)
+      expect(scraped_cats).to be_a(Array)
+      expect(scraped_cats.first).to have_key(:name)
+      expect(scraped_cats.first).to have_key(:breed)
+      expect(scraped_cats.first).to have_key(:gender)
+      expect(scraped_cats.first).to have_key(:age)
+      expect(scraped_cats.first).to have_key(:profile_url)
+      expect(scraped_cats.first).to have_key(:availability)
+    end
+  end
+
+  describe "#scrape_profile" do
+    it "is a class method that scrapes the animal's profile page and returns a hash of attributes describing that animal" do
+      profile_url = "https://www.bluecross.org.uk/pet/luna-1126964?type=adopt"
+      scraped_dog = BlueCrossPets::Scraper.scrape_profile(profile_url)
+      expect(scraped_dog).to be_a(Hash)
+      expect(scraped_dog).to have_key(:bio)
+      expect(scraped_dog).to have_key(:breed_and_colour)
+      expect(scraped_dog).to have_key(:reference)
+      expect(scraped_dog).to have_key(:can_live_with)
+    end
+
+    it "does not scrape information that is not listed on the profile" do
+      profile_url = "https://www.bluecross.org.uk/pet/reggie-1123040?type=adopt"
+      scraped_dog = BlueCrossPets::Scraper.scrape_profile(profile_url)
+      expect(scraped_dog).not_to have_key(:can_live_with)
     end
   end
 end
@@ -45,9 +63,3 @@ end
 #    create dog objects
 #    inherits from Pet class
 #    makes a master list of all dogs
-
-#  Scraper expectations
-#    scrape a list of all dogs/cats from Blue Cross site
-#    for each dog/cat scrape name, breed, gender, age, profile URL, and availability
-#    for each dog/cat scrape additional info including breed & colour, bio, reference #, can live with
-#    use profile url to scrape additional attributes
